c49305d54feae491e83569c38370bd5059c2d2c8
Raik Casimiro <raik.casimiro@g6tec.com.br>
2023-05-28 20:04:19 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, Text } from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Teste</Text>
+    </SafeAreaView>
+  );
+};
+
+// View e Safeview, funcionam mais ou menos da mesma forma que o DIV, porém aqui, o div não pode receber textos nem nada do genero, ele funciona mais como um construtor
+// Text é equivalente ao P
+
+export default App;
