cb43e6c8aafe1607df5c7ac629acdec4a4892784
Raik Casimiro <raik.casimiro@g6tec.com.br>
2023-06-03 15:18:01 -0300

    Criação das pastas para organização, criação base tela de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView } from "react-native";
+
+import Login from "./modules/login/screens/Login";
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>Teste</Text>
+      <Login />
     </SafeAreaView>
   );
 };
-
-// View e Safeview, funcionam mais ou menos da mesma forma que o DIV, porém aqui, o div não pode receber textos nem nada do genero, ele funciona mais como um construtor
-// Text é equivalente ao P
 
 export default App;
