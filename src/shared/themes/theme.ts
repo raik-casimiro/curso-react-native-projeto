fb8966bf20fe144313a5a78ce731313a9baa4c2c
Raik Casimiro <raik.casimiro@g6tec.com.br>
2023-06-03 17:45:11 -0300

    Criação novos temas

--- src/shared/themes/theme.ts
+++ src/shared/themes/theme.ts
@@ -1,8 +1,21 @@
-import { grayTheme, neutralTheme } from "./colors";
+import { buttonsTheme } from "./buttons";
+import {
+  grayTheme,
+  mainTheme,
+  neutralTheme,
+  pinkTheme,
+  purpleTheme,
+} from "./colors";
 
 export const theme = {
   colors: {
     grayTheme,
     neutralTheme,
+    mainTheme,
+    pinkTheme,
+    purpleTheme,
+  },
+  buttons: {
+    buttonsTheme,
   },
 };
